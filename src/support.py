c05d40c24a3bc8a0d71593e1aa987bd85cbaf80e
pauclaret <pauclaretpau@gmail.com>
2023-01-23 14:29:44 +0100

    changes

--- src/support.py
+++ src/support.py
@@ -1,27 +1,19 @@
 import pandas as pd
 import numpy
 
-def read_csv(csv):
+def analyze_dataframe(df):
     """
-    It reads a csv file and returns a pandas dataframe
+    It prints the shape, columns, missing values, data types, head, and describe of a dataframe.
     
-    :param csv: the name of the csv file
-    :return: A dataframe
+    :param df: The dataframe to analyze
     """
-    return pd.read_csv(csv, index_col=0)
+    print("Shape:", df.shape)
+    print("\nColumns:", df.columns)
+    print("\nMissing values:", df.isna().sum())
+    print("\nData types:", df.dtypes)
+    print("\nHead:", df.head())
+    print("\nDescribe:", df.describe())
 
-def index_as_column(dataframe, name):
-    """
-    It takes a dataframe and a name, sets the index name to the name, resets the index, and returns the
-    first five rows of the dataframe
-    
-    :param dataframe: the dataframe you want to convert
-    :param name: The name of the column that will be created from the index
-    :return: The first 5 rows of the dataframe with the index as a column.
-    """
-    dataframe.index = dataframe.index.set_names([name])
-    dataframe = dataframe.reset_index()
-    return dataframe.head()
 
 
     
